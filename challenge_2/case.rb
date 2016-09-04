ae0e4e347dc22a7343a7c51fa14d9f1176a163d9
blokkinen <viktoria_blok@hotmail.com>
2016-09-04 23:06:29 +0200

    Answered third question and deleted the questions I pasted in the rb files

--- challenge_2/case.rb
+++ challenge_2/case.rb
@@ -1,10 +1,4 @@
-(Ruby or/and JavaScript)
-Construct a “case” statement that returns the following:
-- “Young” if value of variable “age” is less than 18
-- “Adult” if value of variable “age” is between 18 and 64
-- “Senior” if value of variable “age” is more than 64
 
----------------------------
 if age => 18
   puts "Young"
 elsif age =< 64
