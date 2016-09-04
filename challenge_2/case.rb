9baf29df0333a0693a4f49fb0429f7c53434b3a3
blokkinen <viktoria_blok@hotmail.com>
2016-09-04 23:28:28 +0200

    forgot to def age in question 2

--- challenge_2/case.rb
+++ challenge_2/case.rb
@@ -1,8 +1,10 @@
+def age(number)
 
-if age => 18
-  puts "Young"
-elsif age =< 64
-  puts "Senior"
-else
-  puts "Adult"
+  if age => 18
+    puts "Young"
+  elsif age =< 64
+    puts "Senior"
+  else
+    puts "Adult"
+  end
 end
