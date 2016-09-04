4564ae35023a11bf4d1f9a6dee916254337b1992
Amber <amber@amberwilkie.com>
2016-09-04 19:44:01 +0200

    Allow for users not to choose a page to cite

--- challenge_5/book.rb
+++ challenge_5/book.rb
@@ -1,20 +1,39 @@
+require 'pry'
 class Book
 
-  attr_accessor :author, :title, :publisher, :year
+  attr_accessor :author, :title, :publisher, :year, :total_pages, :citation
 
  def initialize(attrs)
    self.author = attrs[:author]
    self.title = attrs[:title]
    self.publisher = attrs[:publisher]
    self.year = attrs[:year]
+   self.total_pages = attrs[:total_pages]
  end
 
- def citate(page)
-   if page < 999
-     "#{self.author}, #{self.title}, #{self.publisher}, #{self.year}, #{page}"
-   else
-     raise "Get Bent"
+ def citate(attrs)
+   if attrs.nil? || (attrs == {}) || (attrs == "")
+     attrs = {page: "no page given"}
    end
+  #  if # must now test if 'page' is a string but not "no page given" Or up top there could put 'if page is not an integer'
+  if self.author.nil?
+    raise "no author found"
+  elsif self.title.nil?
+    raise "no title found"
+  elsif ((attrs[:page] == "no page given")) ||
+    ((attrs[:page].kind_of? Integer) && (attrs[:page] <= self.total_pages))
+    @citation = "#{self.author}, #{self.title}, "
+      unless self.publisher.nil?
+        @citation += "#{self.publisher}, "
+      end
+      unless self.year.nil?
+        @citation +=  "#{self.year}, "
+      end
+    @citation += "#{attrs[:page]}."
+    @citation
+  else
+    raise "Get Bent"
+  end
  end
 
 end
--- challenge_5/spec/book_spec.rb
+++ challenge_5/spec/book_spec.rb
@@ -3,7 +3,7 @@ require './book'
 describe Book do
 
   before do
-    @book = Book.new(author: "Amber", title: "Testing environments and passing params and inputs", publisher: "Amber Bo Bamber", year: "1984")
+    @book = Book.new(author: "Amber", title: "Testing environments and passing params and inputs", publisher: "Amber Bo Bamber", year: "1984", total_pages: 1000)
   end
 
   it 'should have an author' do
@@ -15,11 +15,71 @@ describe Book do
   end
 
   it 'should provide a citation of the right page' do
-    expect(@book.citate(50)).to eq "Amber, Testing environments and passing params and inputs, Amber Bo Bamber, 1984, 50"
+    expect(@book.citate(page: 50)).to eq "Amber, Testing environments and passing params and inputs, Amber Bo Bamber, 1984, 50."
   end
 
   it 'should tell you to "get bent" if you try to cite a page larger than the book' do
-    expect{@book.citate(1000)}.to raise_error "Get Bent"
+    expect{@book.citate(page: 1001)}.to raise_error "Get Bent"
+  end
+
+  it 'should default to "no page given" if no page cited' do
+    expect(@book.citate("")).to eq "Amber, Testing environments and passing params and inputs, Amber Bo Bamber, 1984, no page given."
+  end
+
+  it 'should raise "please enter a page number" if asked to cite something other than a number' do
+    expect {@book.citate("monkeys")}.to raise_error "please enter a page number"
+  end
+
+end
+
+describe "Book no author" do
+
+  before do
+    @book = Book.new(title: "Testing environments and passing params and inputs", year: "1984", total_pages: 1000)
+
+  end
+
+
+  it 'should raise "no author found" if you try to cite a book with no author' do
+    expect{@book.citate(page: 50)}.to raise_error "no author found"
+  end
+
+end
+
+describe "Book no title" do
+
+  before do
+    @book = Book.new(author: "Amber", year: "1984", total_pages: 1000)
+
+  end
+
+
+  it 'should raise "no title found" if you try to cite a book with no title' do
+    expect{@book.citate(page: 50)}.to raise_error "no title found"
+  end
+
+end
+
+describe "Book no publisher" do
+
+  before do
+    @book = Book.new(author: "Amber", title: "Testing environments and passing params and inputs", year: "1984", total_pages: 1000)
+  end
+
+  it 'should allow you to cite a book with no publisher' do
+    expect(@book.citate(page: 50)).to eq "Amber, Testing environments and passing params and inputs, 1984, 50."
+  end
+
+end
+
+describe "Book no year" do
+
+  before do
+    @book = Book.new(author: "Amber", title: "Testing environments and passing params and inputs", total_pages: 1000)
+  end
+
+  it 'should allow you to cite a book with no publisher' do
+    expect(@book.citate(page: 50)).to eq "Amber, Testing environments and passing params and inputs, 50."
   end
 
 end
