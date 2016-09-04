2bd1f69ff83e8ef0b29165590541a91f053793f3
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-04 13:29:58 +0200

    Answered question 1

---
+++ challenge_1/answer1.rb
@@ -0,0 +1,6 @@
+"The simplest form of conditionals are the if-statements (boolean expressions of true/false as conditional statements).
+If something is a special way then there will be a certain action. If not, it will not, or else something else will happen.
+Conditionals could be chained with one or many elseif efter if and before else to mark another branch/branches that can be executed.
+
+Another type of conditionals are switch-statements. They first state a case and action happens with the first stated alternative that
+matches the set case."
