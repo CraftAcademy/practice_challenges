26284bd689498f4c3005042ac28e99d6d3560ba8
blokkinen <viktoria_blok@hotmail.com>
2016-09-11 22:21:43 +0200

    after about 30 mins of finding an syntax error, first tests goes green

--- challenge_5/lib/book.rb
+++ challenge_5/lib/book.rb
@@ -1,4 +1,11 @@
 class Book
-  attr_accessor :pages, :title, :author, :publishing_year, :publisher
+  attr_accessor :author, :title, :publisher, :publishing_year, :pages
 
+  def initialize(attrs)
+    self.author = attrs[:author]
+    self.title = attrs[:title]
+    self.publisher = attrs[:publisher]
+    self.publishing_year = attrs[:publishing_year]
+    self.pages = attrs[:pages]
+  end
 end
--- challenge_5/spec/book_spec.rb
+++ challenge_5/spec/book_spec.rb
@@ -1,8 +1,17 @@
 require './lib/book'
 
-describe 'book' do
+describe Book do
+
+  before(:each) do
+    @book = Book.new(author: "Thomas Ochman", title: "Coding As A Craft", publisher: "Pragmatic Sweden", publishing_year: 2016, pages: 50)
+  end
+
+    it 'should have an author' do
+      expect(@book.author).to eq "Thomas Ochman"
+    end
 
 
-it 'returns a source citation of the book'
-  expect(Book.all).to be truthy
+    it 'returns a source citation of the book' do
+      expect(Book.all).to be truthy
+    end
 end
