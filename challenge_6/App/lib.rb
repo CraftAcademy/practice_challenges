7f3e7ea155634dd8d960a39da097bf67173717c9
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-13 17:35:29 +0200

    Setting up Sinatra app

---
+++ challenge_6/App/lib.rb
@@ -0,0 +1,25 @@
+
+
+
+
+Sweden
+
+Country = ['Stockholm', 'Gothenburg', 'Pretoria']
+City = ['Stockholm', 'Gothenburg', 'Pretoria']
+
+
+
+
+{country: "Sweden", capital: "Stockholm"}
+{country: "South Africa", capital: "Pretoria"}
+{country: "Poland", capital: "Warsaw"}
+
+
+
+  puts Stockholm [:capital] + "is the capital of" + Sweden [:country]
+end
+
+
+- “The capital of Sweden is Stockholm”
+- “The capital of South Africa is Pretoria”
+- “The capital of Poland is Warsaw”
--- challenge_6/answer6.rb
+++
@@ -1,4 +0,0 @@
-Hallo Thomas!
-I just want to build this app seperately, which I will do shortly and create a separate pull request for it.
-Best regards
-Nella
