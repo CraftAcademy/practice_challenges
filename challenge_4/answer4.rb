e9113ea07f5ed6edd5ae9e57e69c8c7addc0b5b0
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-14 12:51:48 +0200

    Test clear on question 4

--- challenge_4/answer4.rb
+++ challenge_4/answer4.rb
@@ -1,14 +1,7 @@
 cities = ['Stockholm']
   cities.each do |cities|
-    puts "Hello #{cities}"
-countries = ['Sweden']
+  countries = ['Sweden']
   countries.each do |countries|
 puts "#{cities} is the capital of #{countries} "
   end
-end
-
-
-names = ['Thomas', 'Kalle', 'Anders']
-names.each do |name|
-  puts "Hello #{name}"
 end
