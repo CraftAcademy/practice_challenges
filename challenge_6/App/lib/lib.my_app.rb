182d4bacb834b419c79e24d8d42e71116efea63d
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-14 19:13:04 +0200

    Correcting answer 6 to get right output

--- challenge_6/App/lib/lib.my_app.rb
+++ challenge_6/App/lib/lib.my_app.rb
@@ -2,8 +2,24 @@ get '/' do
     erb :index
   end
 
-country = ['Sweden', 'South Africa', 'Poland']
-city = ['Stockholm', 'Pretoria', 'Warsaw']
+country = ['Sweden']
+city = ['Stockholm']
+  country.each do |country|
+  city.each do |city|
+    puts "#{city} is the capital of #{country}"
+  end
+end
+
+country = ['South Africa']
+city = ['Pretoria']
+  country.each do |country|
+  city.each do |city|
+    puts "#{city} is the capital of #{country}"
+  end
+end
+
+country = ['Poland']
+city = ['Warsaw']
   country.each do |country|
   city.each do |city|
     puts "#{city} is the capital of #{country}"
