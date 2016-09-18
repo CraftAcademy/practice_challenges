8550e932e4543b880b8007a16ed0858b2d8ced30
Jesper <jesper_gs_@hotmail.com>
2016-09-19 00:00:13 +0200

    added the hashes inside an instance variable

--- challenge_6/lib/sinatra_app.rb
+++ challenge_6/lib/sinatra_app.rb
@@ -2,8 +2,11 @@ require 'sinatra/base'
 
 class TestApp < Sinatra::Base
   get '/' do
+
     sweden = { country: 'Sweden', capital: 'Stockholm'}
     sa = { country: 'South Africa', capital: 'Pretoria'}
     poland = { country: 'Poland', capital: 'Warsaw'}
+
+    @countries = [sweden, sa, poland]
   end
 end
